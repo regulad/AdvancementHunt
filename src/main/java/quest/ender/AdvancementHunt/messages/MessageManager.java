aff7d431deb0a071706fc328bd4a8de8aae8534c
regulad <52430642+regulad@users.noreply.github.com>
2021-07-18 06:49:13 -0600

    Legacy Component Serializer for MessageManager

--- src/main/java/quest/ender/AdvancementHunt/messages/MessageManager.java
+++ src/main/java/quest/ender/AdvancementHunt/messages/MessageManager.java
@@ -3,6 +3,7 @@ package quest.ender.AdvancementHunt.messages;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.title.Title;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -33,21 +34,18 @@ public class MessageManager {
         for (@NotNull Map<?, ?> messageEndpoint : messageEndpoints) {
             final @NotNull String sendType = (String) messageEndpoint.get("send_type");
             final @NotNull String messageToSend = (String) messageEndpoint.get("message");
-            final @NotNull Component messageComponent = Component.text(PlaceholderAPI.setPlaceholders(recipient, messageToSend));
+            final @NotNull Component messageComponent = LegacyComponentSerializer.legacyAmpersand().deserialize(PlaceholderAPI.setPlaceholders(recipient, messageToSend));
             switch (sendType) {
                 case "title" -> {
                     final @NotNull String submessageToSend = messageEndpoint.get("submessage") != null ? (String) messageEndpoint.get("submessage") : "";
-                    final @NotNull Component submessageComponent = Component.text(PlaceholderAPI.setPlaceholders(recipient, submessageToSend));
+                    final @NotNull Component submessageComponent = LegacyComponentSerializer.legacyAmpersand().deserialize(PlaceholderAPI.setPlaceholders(recipient, submessageToSend));
                     final @NotNull Title titleToSend = Title.title(messageComponent, submessageComponent);
                     recipient.showTitle(titleToSend);
                 }
-                case "actionbar" -> {
-                    final boolean snowyArea = switch (recipient.getLocation().getBlock().getBiome()) {
-                        case ICE_SPIKES, SNOWY_BEACH, SNOWY_TAIGA, SNOWY_MOUNTAINS, SNOWY_TAIGA_HILLS, SNOWY_TUNDRA, SNOWY_TAIGA_MOUNTAINS -> true;
-                        default -> false; // We needn't do anything if we aren't in the snow.
-                    }; // Makes it so you can see the action bar in the snow. Very, very hard to see.
-                    recipient.sendActionBar(snowyArea ? messageComponent.color(TextColor.color(0)) : messageComponent);
-                }
+                case "actionbar" -> recipient.sendActionBar(switch (recipient.getLocation().getBlock().getBiome()) {
+                    case ICE_SPIKES, SNOWY_BEACH, SNOWY_TAIGA, SNOWY_MOUNTAINS, SNOWY_TAIGA_HILLS, SNOWY_TUNDRA, SNOWY_TAIGA_MOUNTAINS -> true; // Makes it so you can see the action bar in the snow. Very, very hard to see.
+                    default -> false; // We needn't do anything if we aren't in the snow.
+                } ? messageComponent.color(TextColor.color(0)) : messageComponent);
                 case "chat" -> recipient.sendMessage(messageComponent);
             }
         }
